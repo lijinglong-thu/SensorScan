69f5796684630f9ae3b01c2ddc8024cdd36f6197
lijinglong-thu <lijinlon@gmail.com>
2015-11-27 14:13:54 +0800

    添加注释，增加采样间隔控制框，增加界面数据显示，文件存储添加日期。

--- app/src/main/java/com/example/sensorscan/SensorScanActivity.java
+++ app/src/main/java/com/example/sensorscan/SensorScanActivity.java
@@ -33,7 +33,9 @@ public class SensorScanActivity extends Activity {
     private TextView tvAccData;
     private TextView tvGyroData;
     private TextView tvMagData;
-    private EditText edtFileName;
+    private EditText edtDelay_acc;
+    private EditText edtDelay_gyro;
+    private EditText edtDelay_mag;
     private Button btnScanStart;
     private Button btnScanStop;
 
@@ -59,7 +61,7 @@ public class SensorScanActivity extends Activity {
         btnScanStart.setOnClickListener(btnListener);
         btnScanStop.setOnClickListener(btnListener);
     }
-
+//按键监听
     private Button.OnClickListener btnListener = new Button.OnClickListener()
     {
         public void onClick(View v){
@@ -88,24 +90,30 @@ public class SensorScanActivity extends Activity {
         tvAccData = (TextView)findViewById(R.id.tvAccData);
         tvGyroData = (TextView)findViewById(R.id.tvGyroData);
         tvMagData = (TextView)findViewById(R.id.tvMagData);
-        edtFileName = (EditText)findViewById(R.id.edtFileName);
+        edtDelay_acc = (EditText)findViewById(R.id.delay_acc);
+        edtDelay_gyro = (EditText)findViewById(R.id.delay_gyro);
+        edtDelay_mag = (EditText)findViewById(R.id.delay_mag);
         btnScanStart = (Button)findViewById(R.id.scanStart);
         btnScanStop = (Button)findViewById(R.id.scanStop);
     }
-    long timeStart=0;// the timestamp of the first sample
-    float timestamp;
+    long timeStart=0;// the timestamp of the first sample，因为event.timestamp格式为long，这里是为了保证相减前不丢数据，否则后面间隔可能为负
+    float timestamp;// 距离first sample 的时间间隔
+
+    float accOldtimestamp=0;//该参数为了控制界面显示的间隔  设置为500ms
+    float gyroOldtimestamp=0;
+    float magOldtimestamp=0;
     //Sensor Listener
     private SensorEventListener mySensorListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
             float[] values = event.values;
-            float NStoMS=1.0f/1000000.0f;
+            float NS2MS=1.0f/1000000.0f; //event中timestamp为纳秒，纳秒转为毫秒
             if(timeStart==0){
                 timeStart=event.timestamp;
                 timestamp=0;
             }
             else
-                timestamp=(event.timestamp-timeStart)*NStoMS;
+                timestamp=(event.timestamp-timeStart)*NS2MS;
 
             StringBuilder stringBuilder = new StringBuilder();
             switch(event.sensor.getType()){
@@ -113,8 +121,6 @@ public class SensorScanActivity extends Activity {
                     accValues[0] = values[0];
                     accValues[1] = values[1];
                     accValues[2] = values[2];
-                    //String accData = "X:"+accValues[0]+"\n"+"Y:"+accValues[1]+"\n"+"Z:"+accValues[2]+"\n";
-                    //tvAccData.setText(accData);
                     stringBuilder.append(timestamp);
                     for (int i=0;i < 3;i++)
                     {
@@ -126,13 +132,17 @@ public class SensorScanActivity extends Activity {
                     }catch (Exception e){
                         e.printStackTrace();
                     }
+                    if(accOldtimestamp==0||timestamp-accOldtimestamp>500)//第一次或者间隔大于500ms时显示一次
+                    {
+                        accOldtimestamp=timestamp;
+                        String accData = "X:"+accValues[0]+"\n"+"Y:"+accValues[1]+"\n"+"Z:"+accValues[2]+"\n";
+                        tvAccData.setText(accData);
+                    }
                     break;
                 case Sensor.TYPE_GYROSCOPE:
                     gyroValues[0] = values[0];
                     gyroValues[1] = values[1];
                     gyroValues[2] = values[2];
-                    //String gyroData = "X:"+gyroValues[0]+"\n"+"Y:"+gyroValues[1]+"\n"+"Z:"+gyroValues[2]+"\n";
-                    //tvGyroData.setText(gyroData);
                     stringBuilder.append(timestamp);
                     for (int i=0;i < 3;i++)
                     {
@@ -144,13 +154,16 @@ public class SensorScanActivity extends Activity {
                     }catch (Exception e){
                         e.printStackTrace();
                     }
+                    if(gyroOldtimestamp==0||timestamp-gyroOldtimestamp>500) {
+                        gyroOldtimestamp=timestamp;
+                        String gyroData = "X:"+gyroValues[0]+"\n"+"Y:"+gyroValues[1]+"\n"+"Z:"+gyroValues[2]+"\n";
+                        tvGyroData.setText(gyroData);
+                    }
                     break;
                 case Sensor.TYPE_MAGNETIC_FIELD:
                     magValues[0] = values[0];
                     magValues[1] = values[1];
                     magValues[2] = values[2];
-                    //String magData = "X:"+magValues[0]+"\n"+"Y:"+magValues[1]+"\n"+"Z:"+magValues[2]+"\n";
-                    //tvMagData.setText(magData);
                     stringBuilder.append(timestamp);
                     for (int i=0;i < 3;i++)
                     {
@@ -161,6 +174,11 @@ public class SensorScanActivity extends Activity {
                         doWriteToFile(magFileName,stringBuilder.toString());
                     }catch (Exception e){
                         e.printStackTrace();
+                    }
+                    if(magOldtimestamp==0||timestamp-magOldtimestamp>500) {
+                        magOldtimestamp=timestamp;
+                        String magData = "X:"+magValues[0]+"\n"+"Y:"+magValues[1]+"\n"+"Z:"+magValues[2]+"\n";
+                        tvMagData.setText(magData);
                     }
                     break;
             }
@@ -174,18 +192,21 @@ public class SensorScanActivity extends Activity {
 
     private void doStartScan() throws Exception{
         Log.d(TAG, "Create File");
-        //String dateTime = android.text.format.DateFormat.format("yyyy_MM_dd_hhmm", new java.util.Date()).toString();
-        accFileName = new File(getDirectory(),"accData.txt");
-        gyroFileName = new File(getDirectory(),"gyroData.txt");
-        magFileName = new File(getDirectory(),"magData.txt");
+        //Creat txt files for saving data
+        String dateTime = android.text.format.DateFormat.format("yyyy_MM_dd_hhmm", new java.util.Date()).toString();
+        accFileName = new File(getDirectory(),String.format("acc%s.txt",dateTime));
+        gyroFileName = new File(getDirectory(),String.format("gyro%s.txt",dateTime));
+        magFileName = new File(getDirectory(),String.format("mag%s.txt",dateTime));
 
         Log.d(TAG, "Start Listener");
-        int delayMs50=50; //两次sample间隔时间(ms)，仅作参考，实际可能快，可能慢
-        int delayMs20=20;
-        int delayMs10=10;
-        sensorManager.registerListener(mySensorListener, accSensor, delayMs10);
-        sensorManager.registerListener(mySensorListener, gyroSensor, delayMs10);
-        sensorManager.registerListener(mySensorListener, magSensor, delayMs20);
+        //设置两次sample间隔时间(ms)，仅作参考，实际可能快，可能慢
+        int delayAcc=Integer.parseInt(edtDelay_acc.getText().toString());
+        int delayGyro=Integer.parseInt(edtDelay_gyro.getText().toString());
+        int delayMag=Integer.parseInt(edtDelay_mag.getText().toString());
+        //注册Sensor监听器
+        sensorManager.registerListener(mySensorListener, accSensor, delayAcc);
+        sensorManager.registerListener(mySensorListener, gyroSensor, delayGyro);
+        sensorManager.registerListener(mySensorListener, magSensor, delayMag);
         doNotify("Scan Starting");
     }
 
@@ -216,7 +237,7 @@ public class SensorScanActivity extends Activity {
         out.write(string);
         out.close();
     }
-
+    //简化显示的函数
     public void doNotify(String message) {
         doNotify(message, false);
     }
